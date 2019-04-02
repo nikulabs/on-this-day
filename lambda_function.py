b2059253525196a04305de0d1574285c022a2551
Ben Nikula <nikulabs@gmail.com>
2019-04-02 22:13:51 +0100

    Fix PEP8 whitespace issues

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import json
 import time
 import requests
 
@@ -8,6 +7,7 @@ testing = False
 
 def lambda_handler(event, context):
     return get_on_this_day()
+
 
 def get_on_this_day():
     wiki_data = get_wikipedia_day_data()
@@ -24,7 +24,7 @@ def get_wikipedia_day_data():
         return data["extract"]
 
 
-def process_text( unicode_data ):
+def process_text(unicode_data):
     data_list = unicode_data.splitlines()
     event_list = filter(filter_to_events, data_list)
 
@@ -34,39 +34,46 @@ def process_text( unicode_data ):
     event_list = [" In " + s for s in event_list]
     return event_list
 
+
 def filter_to_events(line):
     if line and line[0].isdigit():
         return True
 
+
 def remove_people_dates(event_list):
     last_event = event_list[-1]
-    index_of_separator =  last_event.index(u'·')
+    index_of_separator = last_event.index(u'·')
     index_of_open_paren = last_event[:index_of_separator].index('(')
     index_of_event_end = last_event[:index_of_open_paren].index('.')
     event_list[-1] = last_event[:index_of_event_end+1]
     return event_list
 
+
 def filter_to_read_words(event_list):
-    #TODO: Implement
+    # TODO: Implement
     replace_words = ['(pictured)']
     return event_list
 
+
 def build_json(on_this_day):
-    return { "uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
-             "updateDate": get_update_date(),
-             "titleText": "On This Day, "+time.strftime("%B %d"),
-             "mainText": "".join(on_this_day),
-             "redirectionUrl": "https://en.wikipedia.org/wiki/"+get_url_title()
-           }
+    return {"uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
+            "updateDate": get_update_date(),
+            "titleText": "On This Day, "+time.strftime("%B %d"),
+            "mainText": "".join(on_this_day),
+            "redirectionUrl": "https://en.wikipedia.org/wiki/"+get_url_title()}
+
 
 def get_update_date():
     return time.strftime('%Y-%m-%dT')+"00:00:00.0Z"
 
+
 def get_url_title():
     return "Wikipedia:Selected_anniversaries/"+get_url_date()
 
+
 def get_url_date():
     return time.strftime("%B_")+time.strftime("%d").lstrip("0")
+
 
 if testing:
     print(get_on_this_day())
