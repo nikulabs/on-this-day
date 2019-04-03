ec3cba8395d5578571df5128c6c7086629032708
Ben Nikula <nikulabs@gmail.com>
2019-04-03 17:31:03 +0100

    Add mock for requests to test parsing.

--- tests/test_on_this_day.py
+++ tests/test_on_this_day.py
@@ -3,6 +3,7 @@ import lambda_function as aut
 import time
 
 import unittest
+from unittest import mock
 
 
 class TestDateRequestMethods(unittest.TestCase):
@@ -20,6 +21,40 @@ class TestDateRequestMethods(unittest.TestCase):
         july17 = time.mktime((2013, 7, 17, 0, 0, 0, 0, 0, 0))
         url_format_date = aut.RequestDate(july17).get_update_format()
         self.assertEqual('2013-07-17T00:00:00.0Z', url_format_date)
+
+
+# This method will be used by the mock to replace requests.get
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({
+        "query": {
+            "pages": {
+                "1234": {
+                    "pageid": 1234,
+                    "ns": 0,
+                    "title": "April 3",
+                    "extract": "This is a test message"
+                }
+            }
+        }
+    }, 200)
+
+
+class TestWikipediaOnThisDay(unittest.TestCase):
+
+    # Same as above
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_get_wikipedia_day_data(self, side_effect):
+        actual_response = aut.get_wikipedia_day_data(aut.RequestDate())
+        expected_response = "This is a test message"
+        self.assertEqual(expected_response, actual_response)
 
 
 if __name__ == '__main__':
