76c618607c90b1d0ebba75a80202c84ce76a9de8
Ben Nikula <nikulabs@gmail.com>
2019-04-03 10:23:44 +0100

    Make use of date object to format strings

--- lambda_function.py
+++ lambda_function.py
@@ -18,19 +18,21 @@ def lambda_handler(event, context):
     return get_on_this_day()
 
 
-def get_on_this_day():
-    wiki_data = get_wikipedia_day_data()
+def get_on_this_day(requested_day):
+    wiki_data = get_wikipedia_day_data(requested_day)
     on_this_day = process_text(wiki_data)
-    feed = build_json(on_this_day)
+    feed = build_json(on_this_day, requested_day)
     return feed
 
 
-def get_wikipedia_day_data():
-    extract_command = "format=json&action=query&prop=extracts&exintro=True&explaintext="
-    wikipedia = "https://en.wikipedia.org/w/api.php?"+extract_command+"&titles="+get_url_title()
-    wiki_data = requests.get(wikipedia).json()
+def get_wikipedia_day_data(requested_day):
+    query_component = "format=json&action=query&prop=extracts&exintro=True&explaintext=&titles="+get_url_title(requested_day)
+    wikipedia_url = "https://en.wikipedia.org/w/api.php?"+query_component
+    wiki_data = requests.get(wikipedia_url).json()
+    extracted = []
     for key, data in wiki_data["query"]["pages"].items():
-        return data["extract"]
+        extracted.append(data["extract"])
+    return extracted[0]
 
 
 def process_text(unicode_data):
@@ -64,23 +66,21 @@ def filter_to_read_words(event_list):
     return event_list
 
 
-def build_json(on_this_day):
+def build_json(on_this_day, requested_day):
     return {"uid": "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
             "updateDate": get_update_date(),
             "titleText": "On This Day, "+time.strftime("%B %d"),
             "mainText": "".join(on_this_day),
-            "redirectionUrl": "https://en.wikipedia.org/wiki/"+get_url_title()}
+            "redirectionUrl": "https://en.wikipedia.org/wiki/"+get_url_title(requested_day)}
 
 
 def get_update_date():
     return time.strftime('%Y-%m-%dT')+"00:00:00.0Z"
 
 
-def get_url_title():
-    return "Wikipedia:Selected_anniversaries/"+get_url_date()
+def get_url_title(request_time):
+    return "Wikipedia:Selected_anniversaries/"+request_time.get_url_format()
 
 
-def get_url_date():
-    return time.strftime("%B_")+time.strftime("%d").lstrip("0")
 
 print(get_on_this_day(RequestDate(time)))
