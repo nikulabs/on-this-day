3318c15ff8f1a30139aa4b3fa7e713381c680356
Ben Nikula <nikulabs@gmail.com>
2019-04-03 14:23:13 +0100

    Add test for time in update format

--- tests/test_on_this_day.py
+++ tests/test_on_this_day.py
@@ -6,10 +6,15 @@ import unittest
 
 
 class TestDateRequestMethods(unittest.TestCase):
-    def test_date_url_format(self):
+    def test_get_url_format(self):
         july17 = time.mktime((2013, 7, 17, 0, 0, 0, 0, 0, 0))
         url_format_date = RequestDate(july17).get_url_format()
         self.assertEqual('July_17', url_format_date)
+
+    def test_get_update_format(self):
+        july17 = time.mktime((2013, 7, 17, 0, 0, 0, 0, 0, 0))
+        url_format_date = RequestDate(july17).get_update_format()
+        self.assertEqual('2013-07-17T00:00:00.0Z', url_format_date)
 
 
 if __name__ == '__main__':
